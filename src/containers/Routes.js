334de3b7e19fc2b2e94a488a7e15129f62a46bec
simon198 <c121549443@gmail.com>
2020-12-16 17:07:20 +0800

    登入/ 註冊切版完成

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,42 +1,39 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import Me from "../Pages/Me";
 //  引入各分頁
-
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         {/* 首頁(課程列表) */}
-
       </Route>
       <Route exact path="/">
         {/* 課程列表 */}
-
       </Route>
       <Route exact path="/">
         {/* 購物車 */}
-
       </Route>
       <Route exact path="/">
         {/* 我的課程 */}
-
       </Route>
-      <Route exact path="/">
+      <Route exact path="/me">
+        <Me />
         {/* 我的帳號 */}
-
       </Route>
       <Route exact path="/">
         {/* 管理後台 */}
-
       </Route>
-      <Route exact path="/">
+      <Route exact path="/register">
+        <Register />
         {/* 註冊 */}
-
       </Route>
-      <Route path="/">
+      <Route path="/login">
+        <Login />
         {/* 登入 */}
-
       </Route>
     </Switch>
   );
